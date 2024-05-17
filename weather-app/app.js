1c450d2ef6b896e48066d1ea46e8243be2f91386
Petur <peturivanov4712@gmail.com>
2024-05-17 11:29:31 +0300

    icons

--- weather-app/app.js
+++ weather-app/app.js
@@ -38,51 +38,163 @@ async function getWeatherData(city) {
 }
 
 function displayWeatherInfo(data) {
+
     const { name: city,
         main: { temp, humidity },
-        weather: [{ description, id }], wind: { speed } } = data;
+        weather: [{ description, id }],
+        wind: { speed },
+        dt: currentTimestamp,
+        sys: { sunset, sunrise }
+    } = data;
 
     const weatherImgElement = document.querySelector('.weather img');
+    const weatherDescription = document.querySelector('.description');
     const tempDisplay = document.querySelector('.temp');
     const cityDisplay = document.querySelector('.cityName');
     const humidityDisplay = document.querySelector('.humidity');
     const windDisplay = document.querySelector('.wind');
 
-    const weatherImg = getWeatherImg(id);
+    const dayTime = getDayTime(currentTimestamp, sunrise, sunset);
+
+    const weatherImg = getWeatherImg(id, dayTime);
     weatherImgElement.setAttribute('src', weatherImg);
+    weatherDescription.textContent = description;
     tempDisplay.textContent = `${Math.round(temp)}\u00B0C`;
     cityDisplay.textContent = city;
     humidityDisplay.textContent = `${humidity}%`;
-    windDisplay.textContent = speed;
+    windDisplay.textContent = `${speed} km/h`;
 
     weatherElement.style.display = 'flex';
 
 }
 
-function getWeatherImg(weatherId) {
+function getDayTime(currentTimestamp, sunrise, sunset) {
 
-    switch (true) {
-        case (weatherId >= 200 && weatherId < 300):
-            return `images/rain.png`;
+    return (currentTimestamp >= sunrise && currentTimestamp < sunset) ? 'day' : 'night'
 
-        case (weatherId >= 300 && weatherId < 500):
-            return `images/drizzle.png`;
+}
 
-        case (weatherId >= 500 && weatherId < 600):
-            return `images/rain.png`;
+function getWeatherImg(weatherId, dayTime) {
 
-        case (weatherId >= 600 && weatherId < 700):
-            return `images/snow.png`;
+    const icons = {
+        'day': {
+            200: 'images/storm.png',
+            201: 'images/storm.png',
+            202: 'images/storm.png',
+            210: 'images/storm.png',
+            211: 'images/storm.png',
+            212: 'images/storm.png',
+            221: 'images/storm.png',
+            230: 'images/storm.png',
+            231: 'images/storm.png',
+            232: 'images/storm.png',
+            300: 'images/day-rain.png',
+            301: 'images/day-rain.png',
+            302: 'images/day-rain.png',
+            310: 'images/day-rain.png',
+            311: 'images/day-rain.png',
+            312: 'images/heavy-rain.png',
+            313: 'images/heavy-rain.png',
+            314: 'images/heavy-rain.png',
+            321: 'images/heavy-rain.png',
+            500: 'images/day-rain.png',
+            501: 'images/day-rain.png',
+            502: 'images/day-rain.png',
+            503: 'images/day-rain.png',
+            504: 'images/day-rain.png',
+            511: 'images/freezing-rain.png',
+            520: 'images/heavy-rain.png',
+            521: 'images/heavy-rain.png',
+            522: 'images/heavy-rain.png',
+            531: 'images/heavy-rain.png',
+            600: 'images/snowy.png',
+            601: 'images/snowy.png',
+            602: 'images/snowy.png',
+            611: 'images/snowy.png',
+            612: 'images/snowy.png',
+            613: 'images/snowy.png',
+            615: 'images/snowy.png',
+            616: 'images/snowy.png',
+            620: 'images/snowy.png',
+            621: 'images/snowy.png',
+            622: 'images/snowy.png',
+            701: 'images/mist.png',
+            711: 'images/mist.png',
+            721: 'images/mist.png',
+            731: 'images/mist.png',
+            741: 'images/mist.png',
+            751: 'images/mist.png',
+            761: 'images/mist.png',
+            762: 'images/mist.png',
+            771: 'images/mist.png',
+            781: 'images/mist.png',
+            800: 'images/clear.png',
+            801: 'images/clouds.png',
+            802: 'images/clouds.png',
+            803: 'images/full-clouds.png',
+            804: 'images/full-clouds.png',
 
-        case (weatherId >= 700 && weatherId < 800):
-            return `images/mist.png`;
+        },
+        'night': {
+            200: 'images/storm.png',
+            201: 'images/storm.png',
+            202: 'images/storm.png',
+            210: 'images/storm.png',
+            211: 'images/storm.png',
+            212: 'images/storm.png',
+            221: 'images/storm.png',
+            230: 'images/storm.png',
+            231: 'images/storm.png',
+            232: 'images/storm.png',
+            300: 'images/rain-night.png',
+            301: 'images/rain-night.png',
+            302: 'images/rain-night.png',
+            310: 'images/rain-night.png',
+            311: 'images/rain-night.png',
+            312: 'images/heavy-rain.png',
+            313: 'images/heavy-rain.png',
+            314: 'images/heavy-rain.png',
+            321: 'images/heavy-rain.png',
+            500: 'images/rain-night.png',
+            501: 'images/rain-night.png',
+            502: 'images/rain-night.png',
+            503: 'images/rain-night.png',
+            504: 'images/rain-night.png',
+            511: 'images/freezing-rain.png',
+            520: 'images/heavy-rain.png',
+            521: 'images/heavy-rain.png',
+            522: 'images/heavy-rain.png',
+            531: 'images/heavy-rain.png',
+            600: 'images/snowy.png',
+            601: 'images/snowy.png',
+            602: 'images/snowy.png',
+            611: 'images/snowy.png',
+            612: 'images/snowy.png',
+            613: 'images/snowy.png',
+            615: 'images/snowy.png',
+            616: 'images/snowy.png',
+            620: 'images/snowy.png',
+            621: 'images/snowy.png',
+            622: 'images/snowy.png',
+            701: 'images/mist.png',
+            711: 'images/mist.png',
+            721: 'images/mist.png',
+            731: 'images/mist.png',
+            741: 'images/mist.png',
+            751: 'images/mist.png',
+            761: 'images/mist.png',
+            762: 'images/mist.png',
+            771: 'images/mist.png',
+            781: 'images/mist.png',
+            800: 'images/clear-night.png',
+            801: 'images/night-clouds.png',
+            802: 'images/cloudy.png',
+            803: 'images/full-clouds.png',
+            804: 'images/full-clouds.png',
+        },
+    };
 
-        case (weatherId == 800):
-            return `images/clear.png`;
-
-        case (weatherId > 800):
-            return `images/clouds.png`;
-    }
+    return icons[dayTime][weatherId]
 
 }
 
